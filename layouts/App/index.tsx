59d8608095092fa20c6d8e7a4d2ad03e17a7419b
cororing <corock.dev@gmail.com>
2021-08-25 12:59:13 +0900

    회원가입, 로그인 라우팅 및 페이지 추가

--- layouts/App/index.tsx
+++ layouts/App/index.tsx
@@ -1,7 +1,18 @@
+import loadable from '@loadable/component';
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router';
+
+const LogIn = loadable(() => import('@pages/Login'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
-  return <div>Hello, world!</div>;
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
 };
 
 export default App;
