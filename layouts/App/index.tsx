e8719964f20b7c825d4cb89325b49e7d0552bf4f
cororing <corock.dev@gmail.com>
2021-08-26 18:38:40 +0900

    Playground 컴포넌트 초기화

--- layouts/App/index.tsx
+++ layouts/App/index.tsx
@@ -1,9 +1,11 @@
+import PlayGround from '@layouts/Playground';
 import loadable from '@loadable/component';
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router';
 
 const LogIn = loadable(() => import('@pages/Login'));
 const SignUp = loadable(() => import('@pages/SignUp'));
+const Playground = loadable(() => import('@layouts/Playground'));
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
       <Redirect exact path="/" to="/login" />
       <Route path="/login" component={LogIn} />
       <Route path="/signup" component={SignUp} />
+      <Route path="/playgrounds" component={Playground} />
+      <Route path="/playgrounds/:playground" component={Playground} />
     </Switch>
   );
 };
