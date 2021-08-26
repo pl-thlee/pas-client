1fef62969953e7924ee9e99c819924a1214a2638
cororing <corock.dev@gmail.com>
2021-08-27 01:53:54 +0900

    JWT 작업 중

--- pages/Login/index.tsx
+++ pages/Login/index.tsx
@@ -1,14 +1,33 @@
-import React, { SyntheticEvent, useCallback } from 'react';
+import React, { SyntheticEvent, useCallback, useState } from 'react';
 import { Button, Form, Input, Label, LinkContainer } from '@pages/SignUp/styles';
 import { Link } from 'react-router-dom';
 import useInputs from '@hooks/useInputs';
 import { LoginContainer, LoginWrapper } from './styles';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
 const LogIn = () => {
   const [state, onChange] = useInputs({ userid: '', password: '' });
   const { userid: userId, password } = state;
+  const [logInError, setLogInError] = useState(false);
 
-  const onSubmit = useCallback((e: SyntheticEvent) => {}, []);
+  const onSubmit = useCallback(
+    (e: SyntheticEvent) => {
+      e.preventDefault();
+
+      axios
+        .post('/api/auth/login', { userId, password }, { withCredentials: true })
+        .then((response: AxiosResponse) => {
+          const { accessToken } = response.data;
+
+          // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
+          axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+        })
+        .catch((error: AxiosError) => {
+          setLogInError(error.response?.data?.statusCode === 401);
+        });
+    },
+    [userId, password],
+  );
 
   return (
     <LoginContainer>
@@ -27,6 +46,7 @@ const LogIn = () => {
             <div>
               <Input type="password" id="password" name="password" value={password} onChange={onChange} />
             </div>
+            {logInError && <div>아이디 또는 비밀번호가 일치하지 않습니다.</div>}
           </Label>
 
           <Button type="submit" style={{ border: '1px solid #ccc', marginTop: '1rem' }}>
