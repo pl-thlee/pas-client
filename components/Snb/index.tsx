0b89492e017a918cc99187a1c4e0c3ef32531b72
cororing <corock.dev@gmail.com>
2021-08-31 14:29:00 +0900

    Update styles
    - 테두리 추가

--- components/Snb/index.tsx
+++ components/Snb/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Snb = () => {
-  return <div style={{ background: '#012326', width: '4rem' }}>Side Navigation Bar</div>;
+  return (
+    <div style={{ background: 'rgb(30, 34, 40)', width: '4rem', borderRight: '1px rgb(68, 76, 86) solid' }}>
+      Side Navigation Bar
+    </div>
+  );
 };
 
 export default Snb;
