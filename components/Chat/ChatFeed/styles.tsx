c4bf0a8abee280cfaa21ecc073b16d0c870877b0
DESKTOP-N73DN8E\a3955 <a39550897@gmail.com>
2021-09-10 20:02:28 +0900

    .

---
+++ components/Chat/ChatFeed/styles.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const MessagesContainer = styled.div`
+`;
+
+export const MessagesList = styled.ol`
+`;
+
+export const MessagesItem = styled.li`
+`;
